e7e0b6d6830ef87fbf3957bab0fde663cf4f9ac4
Aakshay <Aakshay@LAPTOP-S1M675KJ>
2021-09-30 18:10:09 +0530

    Repo Commiting

---
+++ src/main/java/com/codegram/cabapi/repository/CabRepository.java
@@ -0,0 +1,11 @@
+package com.codegram.cabapi.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.codegram.cabapi.domain.Cab;
+
+@Repository
+public interface CabRepository extends CrudRepository<Cab, Long> {
+
+}

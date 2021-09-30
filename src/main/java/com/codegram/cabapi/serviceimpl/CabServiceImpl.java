a0046fe465d70c9d10373a7fdab70b13adf37776
Aakshay <Aakshay@LAPTOP-S1M675KJ>
2021-09-30 18:44:48 +0530

    Second Commit

---
+++ src/main/java/com/codegram/cabapi/serviceimpl/CabServiceImpl.java
@@ -0,0 +1,10 @@
+package com.codegram.cabapi.serviceimpl;
+
+import org.springframework.stereotype.Service;
+
+import com.codegram.cabapi.service.CabService;
+
+@Service
+public class CabServiceImpl implements CabService{
+
+}

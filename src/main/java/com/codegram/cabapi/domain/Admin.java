494f5476808d8a1b85c456f53875487e2d38baec
ADMIN <ADMIN@DESKTOP-CTVND1A>
2021-10-01 11:30:57 +0530

    Admin Commit

---
+++ src/main/java/com/codegram/cabapi/domain/Admin.java
@@ -0,0 +1,31 @@
+package com.codegram.cabapi.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
+public class Admin extends AbstractUser {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int adminId;
+
+	public Admin() {
+
+	}
+
+	public int getAdminId() {
+		return adminId;
+	}
+
+	public void setAdminId(int adminId) {
+		this.adminId = adminId;
+	}
+
+	public Admin(int adminId) {
+		super();
+		this.adminId = adminId;
+	}
+}

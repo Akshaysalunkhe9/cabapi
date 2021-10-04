efe148f9655439fed9929cfebcc2b1ed21694d73
Arnav Saha <sahaarnav3@gmail.com>
2021-10-04 21:54:06 +0530

    config

--- src/main/java/com/codegram/cabapi/config/SecurityConfiguration.java
+++ src/main/java/com/codegram/cabapi/config/SecurityConfiguration.java
@@ -1,5 +1,12 @@
 package com.codegram.cabapi.config;
 
-public class SecurityConfiguration {
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		
+	}
 
 }
